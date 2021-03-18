7b3e2214010693fbbccb5671331d79bb98ab6fcc
Munay <64223718+Munovv@users.noreply.github.com>
2021-03-18 17:08:41 +1000

    Create db.sql
    Add SQL file for MySQL and Postgre

---
+++ src/db.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost:8889
+-- Время создания: Мар 18 2021 г., 07:06
+-- Версия сервера: 5.7.26
+-- Версия PHP: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- База данных: `kika_urpl_io_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `i_url_data`
+--
+
+CREATE TABLE `i_url_data` (
+  `id` int(11) NOT NULL,
+  `url` varchar(255) NOT NULL COMMENT 'Входящий URL',
+  `short_code` varchar(255) NOT NULL COMMENT 'Код для редиректа',
+  `date_time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `i_url_data`
+--
+ALTER TABLE `i_url_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `i_url_data`
+--
+ALTER TABLE `i_url_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
